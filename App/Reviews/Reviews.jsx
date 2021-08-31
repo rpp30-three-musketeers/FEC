53f1c56b334feac431f618f9cb21ed6c995a45e7
ErnestoOrtegaHernandez <ernesto.ortega53@gmail.com>
2021-08-31 07:41:06 -0400

    Completed leftCol Skeleton and working on RightColumn

--- App/Reviews/Reviews.jsx
+++ App/Reviews/Reviews.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Reviews.css';
+import LeftColumn from './LeftColumn.jsx';
 
 class Reviews extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  // componentDidMount(){
+
+  // }
 
   render() {
     return (
       <div id={'reviews'}>
         <p>Ratings and Reviews</p>
         <div id={'content'}>
-          <div id={'leftColumn'}>Star</div>
+          <div id={'leftColumn'}>
+            <LeftColumn rating = {3.5} percentRecommend = {91}/>
+          </div>
           <div id={'rightColumn'}>comments</div>
         </div>
 
