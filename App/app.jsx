154ba953b4d9372e00a2ebba96d73346ab47b6b3
Drew Payton <payton.drew@gmail.com>
2021-09-04 15:59:11 -0500

    Gets current product id from URL when root component mounts and sets it to state under the currentProductId key.  Also adds eslint config files to .gitignore list

--- App/app.jsx
+++ App/app.jsx
@@ -5,12 +5,27 @@ import './css/global.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
+
+    this.productIdExtractor = this.productIdExtractor.bind(this);
 
   }
+
+  productIdExtractor(url) {
+    var productId = url.split('/')[3];
+    this.setState({currentProductId: productId});
+  }
+
+  componentDidMount() {
+    this.productIdExtractor(window.location.href);
+  }
+
   render() {
     return (
-      <div><p>Hello World</p></div>
+      <div>
+        <p>Hello World</p>
+        <p>{window.location.href}</p>
+      </div>
     );
   }
 }
