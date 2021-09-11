5751d59c6318a1d0e2424cf43cd6602c73f1b6f6
JK0hl <joshdkohl@gmail.com>
2021-09-11 15:44:25 -0400

    Created skeletons for outfit / related components in app.jsx

---
+++ App/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      <div id="outfit-window">
+        <a>Related Products</a>
+
+      </div>
+
+    </div>
+    )
+  }
+}
+
+export default RelatedProducts;
--- App/app.jsx
+++ App/app.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import './css/global.css';
 import Header from './Header.jsx';
+import RelatedProducts from './RelatedProducts/RelatedProducts.jsx'
+import Outfit from './RelatedProducts/Outfit.jsx'
 import $ from 'jquery';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -27,8 +30,8 @@ class App extends React.Component {
       product_id: null, //select a specific item by id
       endpoint: null, //null, styles, related
       parameters: { //if retrieving all products controls the amount returned
-        page: 1, //default is 1
-        count: 5 //default is 5
+        page: null, //default is 1
+        count: null //default is 5
       }
     }
 
@@ -43,6 +46,8 @@ class App extends React.Component {
         <Header />
         <p>Hello World</p>
         <button type='submit' onClick={this.testCall}>Poke the API</button>
+        <RelatedProducts />
+        <Outfit />
       </div>
     );
   }
