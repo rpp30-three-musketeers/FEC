902eeb3d90f9e20c84364b6210e26663a40434c5
Drew Payton <payton.drew@gmail.com>
2021-09-13 23:28:47 -0700

    Adds description and features text

--- App/Overview/Features.jsx
+++ App/Overview/Features.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class Features extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidUpdate() {
+    console.log('features: ' + this.props.features);
+    this.render();
+  }
 
   render() {
-    return <div id={'features-container'}>
-      <p>Features Component</p>
-      <ul>
-        <li>Feature 1</li>
-        <li>Feature 2</li>
-        <li>Feature 3</li>
-        <li>Feature 4</li>
-        <li>Feature 5</li>
-      </ul>
-    </div>;
+    if (this.props.features) {
+      return <div id={'features-container'}>
+        {this.props.features.map((feature) => {return <p>✔ {feature.feature}: {feature.value}</p>})}
+      </div>;
+    } else {
+      return null;
+    }
   }
 }
 
