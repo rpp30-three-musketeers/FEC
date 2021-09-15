ec0ccbd616094841b2aea7d2389573280026546e
JK0hl <joshdkohl@gmail.com>
2021-09-15 14:39:57 -0400

    Render relatedProduct info from server response data

--- App/RelatedProducts/ProductCard.jsx
+++ App/RelatedProducts/ProductCard.jsx
@@ -19,11 +19,10 @@ class Product extends React.Component {
   getStyle() {
     // eslint-disable-next-line camelcase
     $.get('/products', {product_id: this.props.id, endpoint: 'styles'}, (data) => {
-      console.log('styleData: ', data);
-      // this.setState({
-      //   price: data.results[0].original_price,
-      //   img: data.results[0].photos[0].url
-      // });
+      this.setState({
+        price: data.results[0].original_price,
+        img: data.results[0].photos[0].url
+      });
     });
   }
 
@@ -42,12 +41,12 @@ class Product extends React.Component {
       <div>
         <div id="product-card">
           <div id="product-card-img">
-            <img id="image" src='https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80'/>
+            <img id="image" src={this.state.img}/>
           </div>
           <div id="product-card-attributes">
-            <p id="product-card-category">Jackets</p>
-            <p id="product-card-name" >Camo Onesie</p>
-            <p id="product-card-price">$140</p>
+            <p id="product-card-category">{this.state.category}</p>
+            <p id="product-card-name" >{this.state.name}</p>
+            <p id="product-card-price">{this.state.price}</p>
             <p id="product-card-rating">***__</p>
           </div>
         </div>
