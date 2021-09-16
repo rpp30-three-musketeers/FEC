d1d4b5b98e0b4948ab902ef6fd11c5f80aadd792
ErnestoOrtegaHernandez <ernesto.ortega53@gmail.com>
2021-09-16 16:26:22 -0400

    Added base review and rightColumn tests, fixed header.jsx

--- App/Header.jsx
+++ App/Header.jsx
@@ -1,16 +1,16 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Header extends react.Component {
+class Header extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div id={'site-header'}>
+    return (<div id={'site-header'}>
       <p>Logo Here</p>
       <p>Search Here</p>
-    </div>;
+    </div>);
   }
 }
 
--- App/Reviews/RightColumn.test.js
+++ App/Reviews/RightColumn.test.js
@@ -5,11 +5,12 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import RightColumn from '../Reviews/RightColumn';
+import RightColumn from './RightColumn';
 
 describe('RightColumn Functionality', function() {
   it('should render without throwing an error', function() {
-
+    render(<RightColumn reviewCount = {10} sortedBy = {'Relevance'} />);
+    expect(screen.getByText('10 reviews, sorted by Relevance')).toBeInTheDocument();
   });
 
 });
