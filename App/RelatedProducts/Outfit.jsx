83f4227502d82dfafb77ebd3600a6a8a9e5f7e06
Josh Kohl <joshdkohl@gmail.com>
2021-09-21 19:21:02 -0400

    Create fx() to add overview product to outfit

--- App/RelatedProducts/Outfit.jsx
+++ App/RelatedProducts/Outfit.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Product from './ProductCard.jsx';
+import ProductIdContext from '../context.jsx';
 
 class Outfit extends React.Component {
   constructor(props) {
@@ -10,10 +11,14 @@ class Outfit extends React.Component {
     this.getOutfit = this.getOutfit.bind(this);
     this.setOutfit = this.setOutfit.bind(this);
     this.loadOutfit = this.loadOutfit.bind(this);
+    this.addToOutfit = this.addToOutfit.bind(this);
+    this.removeFromOutfit = this.removeFromOutfit.bind(this);
   }
 
+  static contextType = ProductIdContext;
+
   componentDidMount() {
-    this.setOutfit(47452);
+    // this.setOutfit(47452);
     this.setOutfit(47489);
     this.setOutfit(47474);
     this.getOutfit();
@@ -30,6 +35,23 @@ class Outfit extends React.Component {
     this.setState({
       outfit: storedOutfit
     });
+  }
+
+  addToOutfit() {
+    console.log(this.context);
+    // let currentOutfit = JSON.parse(localStorage.outfit);
+    // currentOutfit.push(this.context);
+    // localStorage.clear();
+    // localStorage.outfit = JSON.stringify(currentOutfit);
+    let currentOutfit = this.state.outfit;
+    currentOutfit.push(this.context);
+    this.setState({
+      outfit: currentOutfit
+    })
+  }
+
+  removeFromOutfit() {
+
   }
 
   loadOutfit() {
@@ -49,12 +71,9 @@ class Outfit extends React.Component {
         <div id="outfit-window">
           <div id="add-to-outfit">
             <p>Add To Outfit</p>
+            <button onClick={this.addToOutfit}>Add To Outfit</button>
           </div>
-
           {this.loadOutfit()}
-          {/* {this.state.outfit.slice(0, 3).map(item => {
-            return <Product id={item} key={item}/>;
-          })} */}
         </div>
 
       </div>
