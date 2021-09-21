1f6a33600c144ec1a0feeabfb83e1892a48b3afd
Josh Kohl <joshdkohl@gmail.com>
2021-09-21 19:48:06 -0400

    Improve addToOutfit function to add to localStorage

--- App/RelatedProducts/Outfit.jsx
+++ App/RelatedProducts/Outfit.jsx
@@ -13,15 +13,24 @@ class Outfit extends React.Component {
     this.loadOutfit = this.loadOutfit.bind(this);
     this.addToOutfit = this.addToOutfit.bind(this);
     this.removeFromOutfit = this.removeFromOutfit.bind(this);
+    this.createOutfitStorage = this.createOutfitStorage.bind(this);
   }
 
   static contextType = ProductIdContext;
 
   componentDidMount() {
     // this.setOutfit(47452);
-    this.setOutfit(47489);
-    this.setOutfit(47474);
+    // this.setOutfit(47489);
+    // this.setOutfit(47474);
+    this.createOutfitStorage();
     this.getOutfit();
+  }
+
+  createOutfitStorage() {
+    if(!localStorage.outfit) {
+      localStorage.outfit = JSON.stringify([]);
+    }
+    return;
   }
 
   setOutfit(target) {
@@ -32,26 +41,37 @@ class Outfit extends React.Component {
 
   getOutfit() {
     let storedOutfit = JSON.parse(localStorage.outfit);
+
     this.setState({
       outfit: storedOutfit
     });
   }
 
   addToOutfit() {
-    console.log(this.context);
-    // let currentOutfit = JSON.parse(localStorage.outfit);
-    // currentOutfit.push(this.context);
-    // localStorage.clear();
-    // localStorage.outfit = JSON.stringify(currentOutfit);
     let currentOutfit = this.state.outfit;
-    currentOutfit.push(this.context);
-    this.setState({
-      outfit: currentOutfit
-    })
+    let newProduct = JSON.parse(this.context);
+    //add product to outfit if not already present
+    if (!currentOutfit.includes(newProduct)){
+      currentOutfit.push(newProduct);
+      localStorage.outfit = JSON.stringify(currentOutfit);
+      this.getOutfit();
+    }
   }
 
   removeFromOutfit() {
+    let currentOutfit = this.state.outfit;
+    let targetProduct = JSON.parse(this.context);
 
+    if (currentOutfit.includes(targetProduct)){
+      //remove target product from outfit list
+      currentOutfit = currentOutfit.filter((item) => {
+        return item !== targetProduct;
+      })
+      //update state with filtered outfit list
+      this.setState({
+        outfit: currentOutfit
+      })
+    }
   }
 
   loadOutfit() {
@@ -72,6 +92,7 @@ class Outfit extends React.Component {
           <div id="add-to-outfit">
             <p>Add To Outfit</p>
             <button onClick={this.addToOutfit}>Add To Outfit</button>
+            <button onClick={this.removeFromOutfit}>Remove From Outfit</button>
           </div>
           {this.loadOutfit()}
         </div>
