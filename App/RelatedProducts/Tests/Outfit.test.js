f72a6b4169e9af9d4661aca32b2d64ef3762eea2
Josh Kohl <joshdkohl@gmail.com>
2021-09-20 20:38:25 -0400

    Created Server testFile with placeholder test

--- App/RelatedProducts/Tests/Outfit.test.js
+++ App/RelatedProducts/Tests/Outfit.test.js
@@ -6,14 +6,16 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import Product from '../ProductCard.jsx';
-import RelatedProducts from '../RelatedProducts.jsx';
+import Outfit from '../Outfit.jsx';
 import data from './sampleData.js';
+
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Related Products Window', () => {
 
-  it('should should run a placholder test', function() {
-
-    expect(true).toBe(true);
+  it('should render without throwing an error', function() {
+    render(<Outfit/>);
+    expect(screen.getByTestId('outfit-window'));
   });
 });
