1a51731c46b3697861e77dca50322b1b7fdda8c3
Drew Payton <payton.drew@gmail.com>
2021-09-22 18:46:16 -0700

    Able to console log the DOM element tag when clicking on an IMG or BUTTON tag

--- App/app.jsx
+++ App/app.jsx
@@ -11,6 +11,94 @@ import $ from 'jquery';
 import { ProductIdProvider } from './context.jsx';
 
 
+const withTracking = eventName => Component => props => (
+  <Track eventName={eventName}>
+    <Component {...props} parentComponent={Component.name} />
+  </Track>
+);
+
+const FunctionButton = props => {
+  return <button>{props.name}</button>;
+};
+
+const ClassButton = class extends React.Component {
+  render() {
+    return <button>{this.props.name}</button>;
+  }
+};
+
+class Track extends React.Component {
+  handleEvent = e => {
+    if (this.props.eventName) {
+      if (e.target.nodeName === 'IMG' || e.target.nodeName === 'button') {
+        console.log('e path: ')
+        console.dir(e.target.nodeName);
+      }
+      // console.log("TRACK", this.props.eventName);
+    }
+  };
+
+  handleChildMounted = (el, child) => {
+    const DOMNode = ReactDOM.findDOMNode(el);
+    if (DOMNode) {
+      DOMNode.addEventListener("click", this.handleEvent);
+    }
+    if (typeof child.ref === "function") {
+      child.ref(el);
+    }
+  };
+
+  wrapWithClass = comp =>
+    class extends React.Component {
+      render() {
+        return comp;
+      }
+    };
+
+  remapChildren(children) {
+    return React.Children.map(children, child => {
+      const ref = el => this.handleChildMounted(el, child);
+
+      // DOM Component, such as:
+      // <button />
+      if (typeof child.type === "string") {
+        console.log(child.props.children);
+        return React.cloneElement(child, { ref });
+
+        // Custom Component w/props.children, such as:
+        // <MyComponent ... />
+        //   <.../>
+        //   <.../>
+        // </MyComponent>
+      }
+
+      else if (React.Children.count(child.props.children)) {
+        return React.cloneElement(child, {
+          children: this.remapChildren(child.props.children)
+        });
+
+        // Custom Class Component w/o props.children, such as:
+        // <MyClassComponent ... />
+      } else if (child.type.prototype.render) {
+        return React.cloneElement(child, { ref });
+
+        // Custom Function Component w/o props.children, such as:
+        // <MyFunctionComponent ... />
+      } else {
+        return React.createElement(this.wrapWithClass(child), { ref });
+      }
+    });
+  }
+
+  render() {
+    return this.remapChildren(this.props.children);
+  }
+}
+
+function Wrapper(props) {
+  return props.children;
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -97,18 +185,22 @@ class App extends React.Component {
   render() {
     let renderReviews = this.state.productReviews.length === 0 ? false : true;
     return (
-      <ProductIdProvider value={window.location.href.split('/')[3]}>
-        <div>
-          <Header />
-          <Overview/>
-          <RelatedProducts/>
-          <Outfit />
-          {renderReviews ? <Reviews data = {this.state.productReviews} avg = {this.state.averageRating}/> : null}
-          <button type='submit' onClick={this.testCall}>Poke the API</button>
-        </div>
-      </ProductIdProvider>
+      <Track>
+        <ProductIdProvider value={window.location.href.split('/')[3]}>
+          <div>
+            <Header />
+            <Overview/>
+            <RelatedProducts/>
+            <Outfit />
+            {renderReviews ? <Reviews data = {this.state.productReviews} avg = {this.state.averageRating}/> : null}
+            <button type='submit' onClick={this.testCall}>Poke the API</button>
+          </div>
+        </ProductIdProvider>
+      </Track>
     );
   }
 }
+
+App = withTracking("elemClick")(App)
 
 ReactDOM.render(<App/>, document.getElementById('app'));
