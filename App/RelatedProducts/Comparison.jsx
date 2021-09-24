d39fae048537eb1b23b140bd79dc7c9690a3302f
Josh Kohl <joshdkohl@gmail.com>
2021-09-23 20:09:15 -0400

    CompareFeatures function to merge data from overview and related product into a single data object

--- App/RelatedProducts/Comparison.jsx
+++ App/RelatedProducts/Comparison.jsx
@@ -3,12 +3,74 @@ import React from 'react';
 class Comparison extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      featuresList: undefined
+    }
+    this.compareFeatures = this.compareFeatures.bind(this);
   }
 
-  //pass current via props
-  //pass related via props
+  componentDidMount() {
+    this.compareFeatures();
+  }
 
+  //Combines features from the overview and related product into one data object based on feature type
+  compareFeatures() {
+    let mainFeatures = this.props.mainProduct.features;
+    let relatedFeatures = this.props.relatedProduct.features;
+    let combinedFeatures = [];
 
+    //adds overview product data from props to container variable
+    mainFeatures.forEach((item) => {
+      let node = {};
+      node.feature = item.feature;
+
+      //if feature does not have discreet value assigns boolean
+      if(item.value === null) {
+        node.mainValue = true;
+      } else {
+        node.mainValue = item.value;
+      }
+      node.relatedValue = false;
+      combinedFeatures.push(node);
+    })
+
+    //iterates through related features prop list
+    relatedFeatures.forEach((item) => {
+      let targetFeature = item.feature;
+      var featureIsShared = false;
+      //compare each feature in related to container array. If present assign related value
+      for (var i = 0; i < combinedFeatures.length; i++) {
+        if (combinedFeatures[i].feature === targetFeature) {
+          featureIsShared = true;
+          if(item.value === null) {
+            combinedFeatures[i].relatedValue = true;
+          } else {
+            combinedFeatures[i].relatedValue = item.value;
+          }
+          return;
+        }
+
+      }
+      //if related feature not present, add with overvalue set to false
+      if(!featureIsShared){
+        let node = {};
+        node.feature = item.feature;
+        if(item.value === null) {
+          node.relatedValue = true;
+        } else {
+          node.relatedValue = item.value;
+        }
+        node.mainValue = false;
+        combinedFeatures.push(node);
+      }
+    });
+
+    console.log('combinedFeatures', combinedFeatures);
+    //update State...
+    this.setState({
+      featuresList: combinedFeatures
+    })
+  }
 
   render() {
     return(
