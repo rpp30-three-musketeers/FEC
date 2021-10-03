607e58e49b3bd01f88548790cb61ffe9cc000157
Drew Payton <payton.drew@gmail.com>
2021-10-02 18:39:44 -0700

    Adds functionality to extended gallery view arrows

--- App/Overview/Gallery/GalleryModal/GalleryModal.jsx
+++ App/Overview/Gallery/GalleryModal/GalleryModal.jsx
@@ -7,19 +7,56 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 class GalleryModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
+    this.navigateLeft = this.navigateLeft.bind(this);
+    this.navigateRight = this.navigateRight.bind(this);
+    this.renderLeftArrow = this.renderLeftArrow.bind(this);
+    this.renderRightArrow = this.renderRightArrow.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({currentStylePhotos: this.props.currentStylePhotos, selectedPhotoIndex: this.props.selectedPhotoIndex})
+  }
+
+  navigateLeft() {
+    this.setState({selectedPhotoIndex: this.state.selectedPhotoIndex - 1});
+  }
+
+  renderLeftArrow() {
+    if (this.state.selectedPhotoIndex <= 0) {
+      return <div></div>
+    } else {
+      return <div className={'navigation-arrow-container left'}><FontAwesomeIcon icon={faAngleLeft} className={'gallery-modal-arrow'} onClick={this.navigateLeft} /></div>
+    }
+  }
+
+  navigateRight() {
+    this.setState({selectedPhotoIndex: this.state.selectedPhotoIndex + 1});
+  }
+
+  renderRightArrow() {
+    if (this.state.selectedPhotoIndex >= this.state.currentStylePhotos.length - 1) {
+      return <div></div>
+    } else {
+      return <div className={'navigation-arrow-container right'}><FontAwesomeIcon icon={faAngleRight} className={'gallery-modal-arrow'} onClick={this.navigateRight} /></div>
+    }
   }
 
   render() {
-    return (
-      <div id={'gallery-modal'}>
-        <div className={'exit-button'}><FontAwesomeIcon icon={faTimesCircle} onClick={this.props.onClick} /></div>
-        <div className={'expanded-photo-container'}>
-          <div className={'navigation-arrow-container left'}><FontAwesomeIcon icon={faAngleLeft} className={'gallery-modal-arrow'}/></div>
-          <img className={'expanded-photo'} src={this.props.currentStylePhotos[this.props.selectedPhotoIndex].url}></img>
-          <div className={'navigation-arrow-container right'}><FontAwesomeIcon icon={faAngleRight} className={'gallery-modal-arrow'} /></div>
+    if (this.state.currentStylePhotos) {
+      return (
+        <div id={'gallery-modal'}>
+          <div className={'exit-button'}><FontAwesomeIcon icon={faTimesCircle} onClick={this.props.onClick} /></div>
+          <div className={'expanded-photo-container'}>
+            {this.renderLeftArrow()}
+            <img className={'expanded-photo'} src={this.state.currentStylePhotos[this.state.selectedPhotoIndex].url}></img>
+            {this.renderRightArrow()}
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return null;
+    }
   }
 }
 
