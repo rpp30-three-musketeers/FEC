4f64ffd4deee743e048ee27d0a3b49daed9e9ba2
Drew Payton <payton.drew@gmail.com>
2021-10-02 18:15:42 -0700

    Adds non-functional navigation arrows to extended gallery view

--- App/Overview/Gallery/GalleryModal/GalleryModal.jsx
+++ App/Overview/Gallery/GalleryModal/GalleryModal.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 class GalleryModal extends React.Component {
   constructor(props) {
@@ -12,7 +14,9 @@ class GalleryModal extends React.Component {
       <div id={'gallery-modal'}>
         <div className={'exit-button'}><FontAwesomeIcon icon={faTimesCircle} onClick={this.props.onClick} /></div>
         <div className={'expanded-photo-container'}>
+          <div className={'navigation-arrow-container left'}><FontAwesomeIcon icon={faAngleLeft} className={'gallery-modal-arrow'}/></div>
           <img className={'expanded-photo'} src={this.props.currentStylePhotos[this.props.selectedPhotoIndex].url}></img>
+          <div className={'navigation-arrow-container right'}><FontAwesomeIcon icon={faAngleRight} className={'gallery-modal-arrow'} /></div>
         </div>
       </div>
     )
